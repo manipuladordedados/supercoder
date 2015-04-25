9d6a916dd13f9d7ef6e2782ea5c96ef292883884
Valter Nazianzeno <manipuladordedados@gmail.com>
2015-04-25 03:37:02 -0400

    Fix conflit with Python 2

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from setuptools import setup
 
 setup(name="SuperCoder",
-      version="1.0",
+      version="1.0.1",
       description="""Meet and help in the development, improvements and 
       corrections of Open Source software""",
       author="Valter Nazianzeno (manipuladordedados)",
       author_email="manipuladordedados@gmail.com",
       url="https://github.com/manipuladordedados/SuperCoder",
-      license="GNU GPLv2",
+      license="GNU GPLv3",
       packages=["SuperCoder"],
       package_data={'SuperCoder': ["img/*", "LICENSE"]},
       data_files=[("share/applications", ["SuperCoder.desktop"]),
